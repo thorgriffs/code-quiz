6585a84055dd5c668e9dc5ef0a32142cbf7f5522
thorgriffs <22.kelliking@gmail.com>
2020-11-04 22:25:49 -0500

    added divs to index.html.  added psuedocode comments to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// create a var countdownTime = 60; to create the timer that conditionally hides
+
+// create variables to hold the questions and answers and identify the answers that are true/false
+
+// I need some functions here, but not sure what yet.  Will need to loop over questions/answers, 
+// and also conditionally hide divs
