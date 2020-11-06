ab30e5c1078f3f839ff7f543081dc37f85b40fb9
thorgriffs <22.kelliking@gmail.com>
2020-11-06 14:18:19 -0500

    added pseudocoding comments to script.js and fixed formatting of div classes on index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // create a var countdownTime = 60; to create the timer that conditionally hides
 
-// create variables to hold the questions and answers and identify the answers that are true/false
+
 
 // I need some functions here, but not sure what yet.  Will need to loop over questions/answers, 
 // and also conditionally hide divs
@@ -9,19 +9,56 @@
 
 var timerEL = document.getElementById("timer");
 var startingTime = 60
-
+var interval
 
 function showTimer() {
     var startingTime = 60;
-    var interval = setInterval(function() {
+
+        interval = setInterval(function() {
         timerEL.textContent = "Timer: " + startingTime;
         startingTime--;
         
         if (startingTime <= 0) {
             timerEL.textContent = "Timer: 00";
             console.log("You are at zero");
+            clearInterval(interval);
         }
     }, 1000);
 }
 
 showTimer();
+
+
+// create variables to hold the questions and answers and identify the answers that are true/false
+
+var quizQuestions = [
+    {question: "Commonly used data types DO NOT include:", answers: ["strings", ,"booleans", "alerts", "numbers"], correctAnswer: 2}
+    {question: "q2", answers: []}, 
+    {question: "q3", answers: []},
+    {question: "q4", answers: []}
+]
+
+// function to start the quiz
+    // on click of <start quiz> button
+    // set/reset score to zero
+    // start timer (set to 60 seconds)
+    // display the first set of questions/answers
+    // hide the .startQuiz div
+
+// show questions (function?)
+    // display each question and answer set from var quizQuestions
+    // determine if game is at the end to then show the gameOver div
+    // hide the questions div
+
+// gameOver
+    // displays final score
+    // allow user to enter initials to store high score
+    // once entered, hide and then display the highScore screen
+
+// highScore
+    // shows numbered list of initials with high scores
+    // needs to stop at a certain number of rows, and also make sure that if more high scores
+    //  exist than rows, that the rows are in fact showing the highest scores (using local storage)
+    // go back button sends user back to startQuiz div
+    // clear highscores button clears the list of high scores 
+
