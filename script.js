bf66033581869bf48c543946353edbf727391413
thorgriffs <22.kelliking@gmail.com>
2020-11-06 15:56:34 -0500

    started showGameOver function that clears the time, hides the .quizTime div, and shows the .gameOver div

--- script.js
+++ script.js
@@ -3,27 +3,24 @@
 // Timer
 
 var timerEL = document.getElementById("timer");
-var startingTime = 60
+var timeRemaining = 60
 var interval
 
 
 function showTimer() {
-    var startingTime = 60;
-
-        interval = setInterval(function() {
-        timerEL.textContent = "Timer: " + startingTime;
-        startingTime--;
+    interval = setInterval(function() {
+        timerEL.textContent = "Timer: " + timeRemaining;
+        timeRemaining--;        
         
-        if (startingTime <= 0) {
+        if (timeRemaining <= 0) {
             timerEL.textContent = "Timer: 00";
             console.log("You are at zero");
-            clearInterval(interval);
+            
         }
     }, 1000);
 }
 
 showTimer();
-
 
 // create variables to hold the questions and answers and identify the answers that are true/false
 
@@ -31,27 +28,31 @@ var quizQuestions = [
     {question: "Commonly used data types DO NOT include:", 
     answers: ["strings", ,"booleans", "alerts", "numbers"], 
     correctAnswer: 2},
+
     {question: "The condition in an if/else statement is enclosed within _____.", 
     answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
     correctAnswer: 2},
+
     {question: "Arrays in JavaScript can be used to store ____.", 
     answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     correctAnswer: 3},
+
     {question: "String values must be enclosed within ____ when being assigned to variables", 
     answers: ["commas", "curly brackets", "quotes", "parentheses"],
     correctAnswer: 2},
+
     {question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
     correctAnswer: 3}
 ]
 
 // function to start the quiz
-function startQuiz () {
+function quizBegins () {
     // set/reset score to zero
     score = 0;
 
     // start timer (set to 60 seconds)
-    startingTime = 60;
+    timeRemaining = 60;
 
     // display the first set of questions/answers
     questionIndex = 0;
@@ -59,6 +60,11 @@ function startQuiz () {
     // hide the .startQuiz div
     var quizStart = document.querySelector(".startQuiz");
     quizStart.classList.add(".hidden");
+
+    // show the quiz questions and answers
+    var qa = document.querySelector(".quizTime");
+
+    // once answered, display the next question
 
 
 
@@ -76,6 +82,15 @@ function startQuiz () {
     // hide the questions div
 
 // gameOver
+function showGameOver() {
+    clearInterval(interval);
+        // hide the questions div
+        var qa = document.querySelector(".quizTime");
+        qa.classList.remove(".hidden");            
+        // show .gameOver div
+        var showGameOver = document.querySelector(".gameOver");
+        showGameOver.classList.add(".hidden");
+}
     // displays final score
     // allow user to enter initials to store high score
     // once entered, hide and then display the highScore screen
